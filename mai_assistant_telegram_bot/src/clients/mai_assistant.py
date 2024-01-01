888e785af3bc04919e7f957c459a5ca30e561828
Gianfranco Demarco <gianfrademarco@gmail.com>
2024-01-01 17:49:43 +0100

    cleanup: remove websockets ref

--- mai_assistant_telegram_bot/src/clients/mai_assistant.py
+++ mai_assistant_telegram_bot/src/clients/mai_assistant.py
@@ -2,26 +2,12 @@ import os
 from contextlib import asynccontextmanager
 
 import requests
-import websockets
-
 
 class MAIAssistantClient:
-
 
     def __init__(self) -> None:
         self.HOST = os.environ.get('MAI_ASSISTANT_URL', 'localhost:8000')
         self.REST_URL = f"http://{self.HOST}"
-
-
-    @asynccontextmanager
-    async def chat_ws(
-        self
-    ):
-        websocket = await websockets.connect(f"ws://{self.HOST}/chat/ws")
-        try:
-            yield websocket
-        finally:
-            await websocket.close()
 
     def chat(
         self,
@@ -34,13 +20,11 @@ class MAIAssistantClient:
         )
         return response.json()
 
-
     def reset_conversation(
         self,
         chat_id: str
     ) -> None:
         requests.delete(f"{self.REST_URL}/conversations/{chat_id}")
-
 
     def login_to_google(
         self,
