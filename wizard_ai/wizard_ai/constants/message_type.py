98083049c3fea86dc9197d3263284bcfca9b962d
Gianfranco Demarco <gianfrademarco@gmail.com>
2024-02-08 21:04:22 +0100

    ci: remove wrong files

--- wizard_ai/wizard_ai/constants/message_type.py
+++ wizard_ai/wizard_ai/constants/message_type.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
+
 class MessageType(Enum):
-    """This class represents an enumeration of different message types. It is used to define and store the possible values for a message type."""
-    TEXT = 'TEXT'
-    TOOL_START = 'TOOL_START'
-    TOOL_END = 'TOOL_END'
+    TEXT = "TEXT"
+    TOOL_START = "TOOL_START"
+    TOOL_END = "TOOL_END"
