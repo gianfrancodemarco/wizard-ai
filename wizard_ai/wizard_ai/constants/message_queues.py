9ed83925cf34af99af3328a69b0939013f60720d
GitHub Action <action@github.com>
2024-02-08 19:19:51 +0000

    Auto-generated docstrings

--- wizard_ai/wizard_ai/constants/message_queues.py
+++ wizard_ai/wizard_ai/constants/message_queues.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-
 class MessageQueues(Enum):
-    wizard_ai_IN = "wizard_ai_in"
-    wizard_ai_OUT = "wizard_ai_out"
+    """The MessageQueues class is an enumeration that represents different types of message queues. It allows for easy referencing and handling of different message queue types in code."""
+    wizard_ai_IN = 'wizard_ai_in'
+    wizard_ai_OUT = 'wizard_ai_out'
